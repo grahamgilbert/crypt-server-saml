61af4e597651b89a042a6cdb8c125e13c13d8070
Graham Gilbert <graham@grahamgilbert.com>
2022-11-09 19:40:30 -0800

    doh

--- process_build.py
+++ process_build.py
@@ -17,13 +17,11 @@ if tag == "":
 
 print(tag)
 dockerfile_content = """FROM macadmins/crypt-server:{}
-MAINTAINER Graham Gilbert <graham@grahamgilbert.com>
-ENV DJANGO_SAML_VERSION 0.16.11
 
 RUN apk add --no-cache --virtual .build-deps \
     xmlsec-dev xmlsec git gcc libc-dev libpq \
     && pip install --no-cache-dir --upgrade setuptools \
-    && pip install djangosaml2==0.18.1
+    && pip install djangosaml2==1.5.3
 
 COPY attributemaps /home/app/crypt/fvserver/attributemaps
 RUN mv /home/app/crypt/fvserver/urls.py /home/app/crypt/fvserver/origurls.py
